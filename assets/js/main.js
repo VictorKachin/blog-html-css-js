64ef7f0ba70868618df4fb64ea3d48434de3553e
Victor Kachin <victor.kachin@gmail.com>
2022-05-13 01:32:54 +0300

    customizations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,25 @@
 // Grab elements
 const selectedElement = selector => {
-	const element = document.querySelector(selector)
-	if (element) return element
-	throw new Error(
-		`Something went, make sure that ${selector} exists or is typed correctly.`
-	)
-}
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw new Error(
+    `Something went, make sure that ${selector} exists or is typed correctly.`
+  );
+};
 
 // console.log(selectedElement(".navbar"))
 
 // Nav styles on scroll
 const scrollHeader = () => {
-	const headerElement = selectedElement("#header")
-	if (this.scrollY >= 15) {
-		headerElement.classList.add("activated")
-	} else {
-		headerElement.classList.remove("activated")
-	}
-}
+  const headerElement = selectedElement("#header");
+  if (this.scrollY >= 15) {
+    headerElement.classList.add("activated");
+  } else {
+    headerElement.classList.remove("activated");
+  }
+};
 
-window.addEventListener("scroll", scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 // Open menu and search pop-up 41:20
 // https://www.youtube.com/watch?v=Aj7HLsJenVg
