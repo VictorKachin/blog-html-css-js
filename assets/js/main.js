be11a6412c62a78641d9ef8aff0cdfbc28ae8307
Victor Kachin <victor.kachin@gmail.com>
2022-05-13 16:06:10 +0300

    feat: add Open menu and search pop-up + dark-light themes add to local storage

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,25 +3,35 @@ const selectedElement = selector => {
   const element = document.querySelector(selector);
   if (element) return element;
   throw new Error(
-    `Something went, make sure that ${selector} exists or is typed correctly.`
+    `Something went, make sure that ${selector} exists or is typed correctly.`,
   );
 };
 
-// console.log(selectedElement(".navbar"))
+// console.log(selectedElement('.navbar'))
 
 // Nav styles on scroll
 const scrollHeader = () => {
-  const headerElement = selectedElement("#header");
+  const headerElement = selectedElement('#header');
   if (this.scrollY >= 15) {
-    headerElement.classList.add("activated");
+    headerElement.classList.add('activated');
   } else {
-    headerElement.classList.remove("activated");
+    headerElement.classList.remove('activated');
   }
 };
 
-window.addEventListener("scroll", scrollHeader);
+window.addEventListener('scroll', scrollHeader);
 
 // Open menu and search pop-up 41:20
+const menuToggleIcon = selectedElement('#menu-toggle-icon');
+
+const toggleMenu = () => {
+  const mobileMenu = selectedElement('#menu');
+  mobileMenu.classList.toggle('activated');
+  menuToggleIcon.classList.toggle('activated');
+};
+
+menuToggleIcon.addEventListener('click', toggleMenu);
+
 // https://www.youtube.com/watch?v=Aj7HLsJenVg
 
 // Open/Close search from pop-up
@@ -29,5 +39,22 @@ window.addEventListener("scroll", scrollHeader);
 // -- Close the search from popup on ESC keypress
 
 // Switch theme/add to local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectedElement('#theme-toggle-btn');
+const currenTheme = localStorage.getItem('currenTheme');
 
+if (currenTheme) {
+  bodyElement.classList.add('light-theme');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+  bodyElement.classList.toggle('light-theme');
+
+  if (bodyElement.classList.contains('light-theme')) {
+    localStorage.setItem('currenTheme', 'themeActive');
+  } else {
+    localStorage.removeItem('currenTheme');
+  }
+
+});
 // Swiper
