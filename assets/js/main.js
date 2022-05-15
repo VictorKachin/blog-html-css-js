015bdfaa49d9532eb65cb8a3882f3ca8051efa2f
Victor Kachin <victor.kachin@gmail.com>
2022-05-15 19:45:40 +0300

    feat: project is completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,74 +1,90 @@
 // Grab elements
-const selectedElement = (selector) => {
-	const element = document.querySelector(selector);
-	if (element) return element;
-	throw new Error(
-		`Something went, make sure that ${selector} exists or is typed correctly.`
-	);
+const selectElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);
 };
 
-// console.log(selectedElement('.navbar'))
-
-// Nav styles on scroll
+//Nav styles on scroll
 const scrollHeader = () => {
-	const headerElement = selectedElement('#header');
-	if (this.scrollY >= 15) {
-		headerElement.classList.add('activated');
-	} else {
-		headerElement.classList.remove('activated');
-	}
+  const navbarElement = selectElement('#header');
+  if (this.scrollY >= 15) {
+    navbarElement.classList.add('activated');
+  } else {
+    navbarElement.classList.remove('activated');
+  }
 };
 
 window.addEventListener('scroll', scrollHeader);
 
-// Open menu and search pop-up 41:20
-const menuToggleIcon = selectedElement('#menu-toggle-icon');
+// Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+const formOpenBtn = selectElement('#search-icon');
+const formCloseBtn = selectElement('#form-close-btn');
+const searchContainer = selectElement('#search-form-container');
 
 const toggleMenu = () => {
-	const mobileMenu = selectedElement('#menu');
-	mobileMenu.classList.toggle('activated');
-	menuToggleIcon.classList.toggle('activated');
+  const mobileMenu = selectElement('#menu');
+  mobileMenu.classList.toggle('activated');
+  menuToggleIcon.classList.toggle('activated');
 };
 
 menuToggleIcon.addEventListener('click', toggleMenu);
 
-// https://www.youtube.com/watch?v=Aj7HLsJenVg
-
-// Open/Close search from pop-up
-const formOpenBtn = selectedElement('#search-icon');
-const formCloseBtn = selectedElement('#form-close-btn');
-const searchFormContainer = selectedElement('#search-form-container');
-
-formOpenBtn.addEventListener('click', () =>
-	searchFormContainer.classList.add('activated')
-);
-
-formCloseBtn.addEventListener('click', () =>
-	searchFormContainer.classList.remove('activated')
-);
-
-// -- Close the search from popup on ESC keypress
+// Open/Close search form popup
+formOpenBtn.addEventListener('click', () => searchContainer.classList.add('activated'));
+formCloseBtn.addEventListener('click', () => searchContainer.classList.remove('activated'));
+// -- Close the search form popup on ESC keypress
 window.addEventListener('keyup', (event) => {
-	if (event.key === 'Escape')
-		searchFormContainer.classList.remove('activated');
+  if (event.key === 'Escape') searchContainer.classList.remove('activated');
 });
 
 // Switch theme/add to local storage
-const bodyElement = document.body;
-const themeToggleBtn = selectedElement('#theme-toggle-btn');
+const body = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
 const currentTheme = localStorage.getItem('currentTheme');
 
+// Check to see if there is a theme preference in local Storage, if so add the ligt theme to the body
 if (currentTheme) {
-	bodyElement.classList.add('light-theme');
+  body.classList.add('light-theme');
 }
 
-themeToggleBtn.addEventListener('click', () => {
-	bodyElement.classList.toggle('light-theme');
+themeToggleBtn.addEventListener('click', function () {
+  // Add light theme on click
+  body.classList.toggle('light-theme');
 
-	if (bodyElement.classList.contains('light-theme')) {
-		localStorage.setItem('currentTheme', 'themeActive');
-	} else {
-		localStorage.removeItem('currentTheme');
-	}
+  // If the body has the class of light theme then add it to local Storage, if not remove it
+  if (body.classList.contains('light-theme')) {
+    localStorage.setItem('currentTheme', 'themeActive');
+  } else {
+    localStorage.removeItem('currentTheme');
+  }
 });
+
 // Swiper
+const swiper = new Swiper('.swiper', {
+  // How many slides to show
+  slidesPerView: 1,
+  // How much space between slides
+  spaceBetween: 20,
+  // Make the next and previous buttons work
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // Make the pagination indicators work
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  //Responsive breakpoints for how many slides to show at that view
+  breakpoints: {
+    // 700px and up shoes 2 slides
+    700: {
+      slidesPerView: 2,
+    },
+    // 1200px and up shoes 3 slides
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
